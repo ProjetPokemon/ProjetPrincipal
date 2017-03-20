a8940bbcc6e6595cca6f1b9f94abf771f68f1b17
Tititoto <thomas.gercon@gmail.com>
2017-03-20 17:13:27 +0100

    creation dossier

--- vue/VueMenuAdmin.php
+++ vue/VueMenuAdmin.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link href="styleMenuAdmin.css" rel="stylesheet" type="text/css">
+    <link href="../css/styleMenuAdmin.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 
